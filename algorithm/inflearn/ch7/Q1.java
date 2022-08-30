87d92212b05b8399322fd5f1ee95a8c1ad1f82d1
Byeongsu KIM <brightdev.bs@gmail.com>
2022-08-30 16:54:29 +0900

    재귀함수 알고리즘

---
+++ algorithm/inflearn/ch7/Q1.java
@@ -0,0 +1,23 @@
+package algorithm.inflearn.ch7;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        recursive(n);
+    }
+
+    public static void recursive(int n) {
+        if(n == 0) {
+            return;
+        } else {
+            recursive(n - 1);
+            System.out.println(n + " ");
+        }
+    }
+}
