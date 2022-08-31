7fb760e0fb727ea60556ecd0c2d94b36c411335a
Byeongsu KIM <brightdev.bs@gmail.com>
2022-08-31 18:37:53 +0900

    DTO Validation

--- rest-api-practice/src/main/java/com/vanille/restapipractice/inflearn/EventDto.java
+++ rest-api-practice/src/main/java/com/vanille/restapipractice/inflearn/EventDto.java
@@ -9,6 +9,9 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder
@@ -17,14 +20,23 @@ import java.time.LocalDateTime;
 @Data
 public class EventDto {
 
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NotNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
     private LocalDateTime beginEventDateTime;
+    @NotNull
     private LocalDateTime endEventDateTime;
     private String location; // (optional : 없으면 온라인 모임)
+    @Min(0)
     private int basePrice;
+    @Min(0)
     private int maxPrice;
+    @Min(0)
     private int limitOfEnrollment;
 }
